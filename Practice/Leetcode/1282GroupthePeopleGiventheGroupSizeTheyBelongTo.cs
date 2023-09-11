9c78d3107178ad9798327773bd534ca1e3e2ea6a
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-09-11 09:51:57 +0700

    update new sol for 20230911

--- Practice/Leetcode/1282GroupthePeopleGiventheGroupSizeTheyBelongTo.cs
+++ Practice/Leetcode/1282GroupthePeopleGiventheGroupSizeTheyBelongTo.cs
@@ -1,19 +1,25 @@
-public class Solution {
-    public IList<IList<int>> GroupThePeople(int[] groupSizes) {
+public class Solution
+{
+    public IList<IList<int>> GroupThePeople(int[] groupSizes)
+    {
         var groups = new Dictionary<int, List<int>>();
-        for (var i = 0; i< groupSizes.Length; i++) {
-            if (!groups.ContainsKey(groupSizes[i])) {
+        for (var i = 0; i < groupSizes.Length; i++)
+        {
+            if (!groups.ContainsKey(groupSizes[i]))
+            {
                 groups.Add(groupSizes[i], new List<int>());
             }
             groups[groupSizes[i]].Add(i);
         }
 
         var result = new List<IList<int>>();
-        foreach (var group in groups) {
+        foreach (var group in groups)
+        {
             var groupSize = group.Key;
             var groupMembers = group.Value;
             var groupCount = groupMembers.Count / groupSize;
-            for (var i = 0; i <groupCount; i++) {
+            for (var i = 0; i < groupCount; i++)
+            {
                 var groupMembersSubset = groupMembers.GetRange(i * groupSize, groupSize);
                 result.Add(groupMembersSubset);
             }
