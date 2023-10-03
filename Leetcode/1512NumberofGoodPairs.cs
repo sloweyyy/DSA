d1950f1f0589980b104b4978efa7866ff35815ba
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-10-03 07:55:23 +0700

    update

--- Leetcode/1512NumberofGoodPairs.cs
+++ Leetcode/1512NumberofGoodPairs.cs
@@ -5,7 +5,7 @@ public class Solution
         int count = 0;
         for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i+1; j < nums.Length; j++)
+            for (int j = i + 1; j < nums.Length; j++)
             {
                 if (nums[i] == nums[j])
                 {
