bdb3c5f9a16b16451107861f680141c04c740a1e
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-10-04 18:32:51 +0700

    update code như điên 20231004

--- Leetcode/1512NumberofGoodPairs.cs
+++ Leetcode/1512NumberofGoodPairs.cs
@@ -1,18 +1,26 @@
-public class Solution
-{
-    public int NumIdenticalPairs(int[] nums)
-    {
-        int count = 0;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            for (int j = i + 1; j < nums.Length; j++)
-            {
-                if (nums[i] == nums[j])
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
+public class MyHashMap {
+
+    public MyHashMap() {
+        
+    }
+    
+    public void Put(int key, int value) {
+        
+    }
+    
+    public int Get(int key) {
+        
+    }
+    
+    public void Remove(int key) {
+        
     }
 }
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.Put(key,value);
+ * int param_2 = obj.Get(key);
+ * obj.Remove(key);
+ */
