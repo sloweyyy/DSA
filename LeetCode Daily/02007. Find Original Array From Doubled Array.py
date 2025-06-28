5a1ce3f0e7b8cc0172cc06aa9d0ef46982b9e098
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2025-06-29 00:22:11 +0700

    feat: Add solution for "Find Original Array From Doubled Array"

---
+++ LeetCode Daily/02007. Find Original Array From Doubled Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 == 1:
+            return []
+        count = Counter(changed)
+        changed.sort()
+        original = []
+        for num in changed:
+            if count[num] == 0:
+                continue
+            if count[num * 2] == 0:
+                return []
+            original.append(num)
+            count[num] -= 1
+            count[num * 2] -= 1
+        return original
